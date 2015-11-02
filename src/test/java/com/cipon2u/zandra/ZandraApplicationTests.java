a05730118dbf7a1579dc8455e0e1696a138c522e
cipon <cipon@192.168.0.106>
2015-11-02 21:25:49 +0800

    remove unit test

--- src/test/java/com/cipon2u/zandra/ZandraApplicationTests.java
+++ src/test/java/com/cipon2u/zandra/ZandraApplicationTests.java
@@ -6,12 +6,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ZandraApplication.class)
-@WebAppConfiguration
+
 public class ZandraApplicationTests {
 
-	@Test
+
 	public void contextLoads() {
 	}
 
